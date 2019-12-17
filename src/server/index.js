f3db2c29b9588b82b2845509fddeb20258c0bae3
meganrm <meganrm@gmail.com>
2019-12-16 23:34:56 -0800

    added whitelist options for cors

--- src/server/index.js
+++ src/server/index.js
@@ -6,11 +6,21 @@ require('dotenv').config();
 const { saveEvent } = require('../archive-handling/move-event');
 const validateEvent = require('../one-time-scripts/validate-event');
 
+const whitelist = ['http://thp-admin.herokuapp.com', 'https://thp-admin.herokuapp.com', 'http://localhost:3000']
+
 const app = express();
 app.use(express.json());
+
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'http://thp-admin.herokuapp.com' : 'http://localhost:3000',
-}));
+  origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+  }
+}
+));
 
 app.post('/event', (req, res) => {
   const {
